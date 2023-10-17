e922f9c8b808c56dd7b7b624a1c25cc3c8af0077
chirmstream <33538092+chirmstream@users.noreply.github.com>
2023-10-16 17:51:07 -0700

    setup docker engine install

--- installCompose.sh
+++ installCompose.sh
@@ -1,2 +1,28 @@
 #! /bin/bash
 
+# First install docker engine
+    # Remove any existing docker installation
+    sudo yum remove docker \
+        docker-client \
+        docker-client-latest \
+        docker-common \
+        docker-latest \
+        docker-latest-logrotate \
+        docker-logrotate \
+        docker-engine
+
+    # Setup repository
+    sudo yum install -y yum-utils
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+    # Install docker engine
+    sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    # GPG key should match "060A 61C5 1B55 8A7F 742B 77AA C52F EB6B 621E 9F35"
+
+    # Start docker
+    sudo systemctl start docker
+
+    # Verify docker installation
+    sudo docker run hello-world
+
+# Install docker compose
